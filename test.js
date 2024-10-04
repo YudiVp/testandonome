cdf5867d1e66a0125219d6510a22f2a1bdd70d7e
YudiVp <144558868+YudiVp@users.noreply.github.com>
2024-10-04 16:25:47 -0300

    Add files via upload

---
+++ test.js
@@ -0,0 +1,8 @@
+function verificarNome() {
+    const nome = prompt("Digita o nome ae brow");
+    if (nome === "Yudi") {
+        alert("Tu acertou man");
+    } else {
+        alert("Tu errou man")
+    }
+}   
